0ec144c7e5f4f5834bc5951c39ebe30d7f5eecfc
taulant <taulantus@gmail.com>
2020-04-14 04:00:46 +0000

    hide cred

--- util/aws-upload.js
+++ util/aws-upload.js
@@ -3,8 +3,8 @@ const stream = require('stream');
 
 const uploadStream = ({ Bucket, Key }) => {
   aws.config.update({
-    secretAccessKey: 'gZ7Y8Zd5o0CgBYrlJVxjSAJhyUBBOdMgVKprt6sQ',
-    accessKeyId: 'AKIAZFTSOWX3PVEAZKE7',
+    secretAccessKey: '*',
+    accessKeyId: '*',
     region: 'us-east-1',
   });
   const s3 = new aws.S3();
