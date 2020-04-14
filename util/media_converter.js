f99908bcbcb9b092c4cf6f5c66b72c6275b4d20d
Taulant Vokshi <taulantvokshi2@gmail.com>
2020-04-14 12:31:10 -0400

    change

--- util/media_converter.js
+++ util/media_converter.js
@@ -11,7 +11,9 @@ const mediaConverter = (filePath, fileName, callback) => {
     Bucket: 'youtube-converter-mp3-mp4',
     Key: `${exactFileName}.mp3`,
   });
-  ffmpeg.setFfmpegPath('/usr/bin/ffmpeg');
+  ffmpeg.setFfmpegPath(
+    '/app/tmp/buildpacks/17942be262d9aab972d360c2ca93b45c9bf573d42825506b9d54baa47b2c145501307cfa4b7f51ca1d1f45f75acb698a808a6b2b207420a64bcc6fc59e419199/bin/'
+  );
   ffmpeg(filePath)
     .fromFormat('mp4')
     .toFormat('mp3')
