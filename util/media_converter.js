af118b763ad2e4c12f59c8615c3a0136b15cbb0d
Taulant Vokshi <taulantvokshi2@gmail.com>
2020-04-14 12:36:44 -0400

    heroku

--- util/media_converter.js
+++ util/media_converter.js
@@ -12,8 +12,7 @@ const mediaConverter = (filePath, fileName, callback) => {
     Key: `${exactFileName}.mp3`,
   });
   ffmpeg.setFfmpegPath(
-    '/app/tmp/buildpacks/17942be262d9aab972d360c2ca93b45c9bf573d42825506b9d54baa47b2c145501307cfa4b7f51ca1d1f45f75acb698a808a6b2b207420a64bcc6fc59e419199/bin/'
-  );
+    '/app/tmp/buildpacks/bin/ffmpg/',
   ffmpeg(filePath)
     .fromFormat('mp4')
     .toFormat('mp3')
